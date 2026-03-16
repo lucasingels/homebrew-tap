b8368bc27dc4fc0d1b119cead364d9f0d3b960b4
Luca <luca@nile.ag>
2026-03-16 14:06:56 +0200

    sapling-dev 0.1.4: fix bottle stanza

--- Formula/sapling-dev.rb
+++ Formula/sapling-dev.rb
@@ -7,7 +7,7 @@ class SaplingDev < Formula
   version "0.1.4"
 
   bottle do
-    url "https://github.com/lucasingels/sapling/releases/download/v0.1.4/sapling-dev-0.1.4.arm64.bottle.tar.gz"
+    root_url "https://github.com/lucasingels/sapling/releases/download/v0.1.4"
     sha256 cellar: :any, arm64_tahoe:   "1ae1fe4f69542238baf6ccbb61409c0815e8d68cad1d03d5aae3dd4004647fee"
     sha256 cellar: :any, arm64_sequoia: "1ae1fe4f69542238baf6ccbb61409c0815e8d68cad1d03d5aae3dd4004647fee"
     sha256 cellar: :any, arm64_sonoma:   "1ae1fe4f69542238baf6ccbb61409c0815e8d68cad1d03d5aae3dd4004647fee"
