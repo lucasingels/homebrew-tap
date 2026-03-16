9151929e752d9e466988f88d62a9838324ae76d2
Luca <luca@nile.ag>
2026-03-16 13:59:17 +0200

    sapling-dev 0.1.4: add url

--- Formula/sapling-dev.rb
+++ Formula/sapling-dev.rb
@@ -2,6 +2,8 @@ class SaplingDev < Formula
   desc "The Sapling source control client"
   homepage "https://sapling-scm.com"
   license "GPL-2.0-or-later"
+  url "https://github.com/lucasingels/sapling/releases/download/v0.1.4/sapling-dev-0.1.4.arm64.bottle.tar.gz"
+  sha256 "1ae1fe4f69542238baf6ccbb61409c0815e8d68cad1d03d5aae3dd4004647fee"
   version "0.1.4"
 
   bottle do
