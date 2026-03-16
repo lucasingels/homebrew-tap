f24d6edb97cbacae25e39e3198041cd4bdd79adf
Luca <luca@nile.ag>
2026-03-16 13:55:27 +0200

    sapling-dev 0.1.4

---
+++ Formula/sapling-dev.rb
@@ -0,0 +1,16 @@
+class SaplingDev < Formula
+  desc "The Sapling source control client"
+  homepage "https://sapling-scm.com"
+  license "GPL-2.0-or-later"
+  version "0.1.4"
+
+  bottle do
+    root_url "https://github.com/lucasingels/sapling/releases/download/v0.1.4"
+    sha256 cellar: :any, arm64_sequoia: "1ae1fe4f69542238baf6ccbb61409c0815e8d68cad1d03d5aae3dd4004647fee"
+  end
+
+  depends_on "python@3.11"
+  depends_on "node"
+  depends_on "openssl@3"
+  depends_on "gh"
+end
